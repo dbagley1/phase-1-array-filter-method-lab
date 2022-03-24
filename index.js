a9b56a1dccd52e6723c623b90b73807dc4c384d6
Darrian Bagley <dbagle14@gmail.com>
2022-03-23 21:45:08 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(arr, name) {
+    return arr.filter(function (element) {
+        return element.toUpperCase() === name.toUpperCase();
+    });
+}
+
+function fuzzyMatch(arr, letters) {
+    return arr.filter(function (element) {
+        return element.slice(0, letters.length) === letters;
+    });
+}
+
+function matchName(arr, name) {
+    return arr.filter(function (element) {
+        return element.name === name;
+    });
+}
